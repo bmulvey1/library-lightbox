d0bbe26b55376a31a12400975b74c125bc794139
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2025-06-03 16:01:23 -0400

    add attract mode logic, usb brightness adjustment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0114,C0115,R0903,C0200,C0103
+# pylint: disable=C0114,C0115,C0116,R0903,C0200,C0103
 
 # 256 ────────────────┐241
 # 225┌────────────────┘240
@@ -61,10 +61,15 @@ class State:
 SPIRAL_TIME = 0.02  # 20 ms / px = 5.12 sec to finish spiral
 SPIRAL_EXPAND_TIME = 0.2
 
+ATTRACT_FADE_TIME = 3
+
+VBUS_PIN = board.VBUS_SENSE
 PIXEL_PIN = board.GP27
 ACC_BUTTON_PIN = board.GP15
 BIG_BUTTON_PIN = board.GP26
 AUDIO_PIN = board.GP20
+
+USB_CONNECTED = digitalio.DigitalInOut(VBUS_PIN).value
 
 keys = keypad.keys((ACC_BUTTON_PIN, BIG_BUTTON_PIN),
                    value_when_pressed=True, pull=True)
@@ -75,7 +80,8 @@ KEY_BBUTTON = 1
 ROWS = 16
 COLS = 16
 NUM_PIXELS = ROWS*COLS
-BRIGHTNESS = 0.3
+# keep brightness low if usb connected to avoid overloading port
+BRIGHTNESS = 0.5 if not USB_CONNECTED else 0.01
 
 pixels = neopixel.NeoPixel(
     PIXEL_PIN, NUM_PIXELS, pixel_order=neopixel.GRB, brightness=BRIGHTNESS, auto_write=False)
@@ -88,6 +94,14 @@ matrix = neomatrix.NeoMatrix(pixels, ROWS, COLS, 1, 1, matrixType, rotation=0)
 audio = audiopwmio.PWMAudioOut(AUDIO_PIN)
 
 state = State.STANDBY
+
+select_new_flash = False
+current_fade_percentage = 0
+fade_percentage_increment = 0.02
+reverse_fade = False
+
+def set_brightness(color, brightness):
+    return round((color >> 16) * brightness) << 16 | round(((color >> 8) & 0xFF) * brightness) << 8 | round((color & 0xFF) * brightness)
 
 while 1:
 
@@ -175,6 +189,7 @@ while 1:
 
             matrix.display()
             time.sleep(SPIRAL_EXPAND_TIME)
+        state = State.SPIRAL_END
 
     elif state == State.SPIRAL_END:
         if event.pressed & event.key_number == KEY_BBUTTON:
@@ -183,6 +198,33 @@ while 1:
     elif state == State.ATTRACT:
         if event.pressed & event.key_number == KEY_BBUTTON:
             state = State.SPIRAL
+        # select random x, y, color
+        if select_new_flash:
+            coords = (random.randint(2, ROWS-2), random.randint(2, COLS-2))
+            fade_size = random.randint(0,2)
+            fade_color = colors[random.randint(1, 6)]
+            current_fade_percentage = 0
+            reverse_fade = False
+            fade_pixels = list((range(coords[0] - fade_size, coords[0] + fade_size), range(coords[1] - fade_size, coords[1] + fade_size)))
+
+        # fade in and out over ATTRACT_FADE_TIME seconds
+
+        for x in fade_pixels[0]:
+            for y in fade_pixels[1]:
+                matrix.pixel(x, y, set_brightness(fade_color, current_fade_percentage))
+
+        if current_fade_percentage < 1:
+            current_fade_percentage += fade_percentage_increment
+        elif current_fade_percentage == 1:
+            reverse_fade = True
+            fade_percentage_increment = -fade_percentage_increment
+
+        if reverse_fade and current_fade_percentage == 0:
+            reverse_fade = False
+            fade_percentage_increment = -fade_percentage_increment
+            select_new_flash = True
+
+        time.sleep(ATTRACT_FADE_TIME / 100)
 
     elif state == State.ATTRACT_SILENT:
         if event.pressed & event.key_number == KEY_BBUTTON:
